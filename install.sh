a0ef617cc3a4fc3d77d38892341647107edfd87d
c3t4r4 <neocetara@hotmail.com>
2023-04-27 11:45:13 -0300

    Update

---
+++ install.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+CURRENT_USER="${SUDO_USER}"
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+
+welcome_screen() {
+cat << "EOF"
+
+________/\\\\\\\\\_____/\\\\\\\\\\___________________________/\\\_____________________________/\\\____        
+ _____/\\\////////____/\\\///////\\\________________________/\\\\\___________________________/\\\\\____       
+  ___/\\\/____________\///______/\\\______/\\\_____________/\\\/\\\_________________________/\\\/\\\____      
+   __/\\\_____________________/\\\//____/\\\\\\\\\\\______/\\\/\/\\\_____/\\/\\\\\\\_______/\\\/\/\\\____     
+    _\/\\\____________________\////\\\__\////\\\////_____/\\\/__\/\\\____\/\\\/////\\\____/\\\/__\/\\\____    
+     _\//\\\______________________\//\\\____\/\\\_______/\\\\\\\\\\\\\\\\_\/\\\___\///___/\\\\\\\\\\\\\\\\_   
+      __\///\\\___________/\\\______/\\\_____\/\\\_/\\__\///////////\\\//__\/\\\_________\///////////\\\//__  
+       ____\////\\\\\\\\\_\///\\\\\\\\\/______\//\\\\\_____________\/\\\____\/\\\___________________\/\\\____ 
+        _______\/////////____\/////////_________\/////______________\///_____\///____________________\///_____
+
+EOF
+}
+
+check_operating_system() {
+   # Check that this installer is running on a
+   # Debian-like operating system (for dependencies)
+
+   echo -e "\e[39m[+] Checking operating system\e[39m"
+   error="\e[91m    [✘] Need to be run on a Ubuntu-like operating system, exiting.\e[39m"
+
+   if [[ -f "/etc/os-release" ]]; then
+       if [[ $(cat /etc/os-release | grep -e "ID_LIKE=\"\?ubuntu" -e "ID=ubuntu") ]]; then
+           echo -e "\e[92m    [✔] Ubuntu-like operating system\e[39m"
+       else
+           echo -e "$error"
+           exit 1
+       fi
+   else
+       echo -e "$error"
+       exit 1
+   fi
+}
+
+create_directory() {
+    # Create the TinyCheck directory and move the whole stuff there.
+    echo -e "[+] Creating TinyCheck folder under /usr/share/"
+    mkdir /usr/share/c3t4r4
+    cp -Rf ./* /usr/share/c3t4r4
+}
+
+cleaning() {
+    # Removing some files and useless directories
+    rm /usr/share/c3t4r4/install.sh
+    rm /usr/share/c3t4r4/README.md
+    rm /usr/share/c3t4r4/LICENSE.txt
+    rm /usr/share/c3t4r4/NOTICE.txt
+    rm -rf /usr/share/c3t4r4/assets/
+}
+
+update_upgrade() {
+    sudo timedatectl set-timezone America/Sao_Paulo && sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt install curl wget net-tools software-properties-common acl unzip htop ncdu -y
+}
+
+limit_jornalctl() {
+    sudo journalctl --vacuum-time=2d && sudo journalctl --vacuum-size=500M
+}
+
+install_oh_my_z() {
+    sudo apt install zsh fonts-powerline dconf-cli -y && zsh --version
+    chsh -s /usr/bin/zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+    sed 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' $HOME/.zshrc
+    sed 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k/powerlevel10k"/g' $HOME/.zshrc
+
+    echo -e "if [ -f ~/.zshrc_aliases ]; then" > $HOME/.zshrc
+    echo -e "  . ~/.zshrc_aliases" > $HOME/.zshrc
+    echo -e "fi" > $HOME/.zshrc
+    
+    #echo -e "# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh." > $HOME/.zshrc
+    #echo -e "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" > $HOME/.zshrc
+}
+
+set_p10k_config(){
+    cp .p10k.zsh $HOME/
+    cp .zshrc_aliases $HOME/
+    source $HOME/.zshrc
+}
+
+
+remove_libreoffice() {
+    sudo apt-get remove libreoffice-core -y
+}
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This must be run as root. Type in 'sudo bash $0' to run."
+	exit 1
+elif [[ -f /usr/share/c3t4r4/modifyLinux.yaml ]]; then
+    echo "You have a c3t4r4 instance already installed on this box."
+	exit 1
+else
+    welcome_screen
+    check_operating_system
+    limit_jornalctl
+    update_upgrade
+    #create_directory
+    install_oh_my_z
+    set_p10k_config
+    
+fi
