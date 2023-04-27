1b60cc8a5eb56deacdca0df5b190a5cdce29ccc0
c3t4r4 <neocetara@hotmail.com>
2023-04-27 16:57:55 -0300

    Update

--- install.sh
+++ install.sh
@@ -203,8 +203,8 @@ install_edge(){
     if [ "" = "$PKG_OK" ]; then
         
         if ! grep -q https://packages.microsoft.com/repos/edge "/etc/apt/sources.list.d/microsoft-edge-dev.list" && ! grep -q https://packages.microsoft.com/repos/edge "/etc/apt/sources.list.d/microsoft-edge.list"; then
-            curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg > /dev/null 2>&1
-            sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/ > /dev/null 2>&1
+            curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+            sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
             sudo sh -c 'echo "deb [arch=amd64]  https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
             sudo rm microsoft.gpg && sudo apt update > /dev/null 2>&1
         fi
@@ -223,7 +223,7 @@ install_vscode(){
 
     if [ "" = "$PKG_OK" ]; then
         if ! grep -q https://packages.microsoft.com/repos/vscode "/etc/apt/sources.list.d/microsoft-edge-dev.list" && ! grep -q https://packages.microsoft.com/repos/vscode "/etc/apt/sources.list.d/microsoft-edge.list"; then
-            wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add – > /dev/null 2>&1
+            wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add – 
             sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" >> /etc/apt/sources.list.d/microsoft-edge-dev.list'
             sudo apt update > /dev/null 2>&1
         fi
@@ -243,7 +243,7 @@ install_sublime(){
     if [ "" = "$PKG_OK" ]; then
         wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null 2>&1
         sudo sh -c 'echo "deb [arch=amd64] https://download.sublimetext.com/ apt/stable/" >> /etc/apt/sources.list.d/sublime-text.list'
-        sudo apt update && sudo apt install $REQUIRED_PKG -y > /dev/null 2>&1
+        sudo apt update > /dev/null 2>&1 && sudo apt install $REQUIRED_PKG -y > /dev/null 2>&1
         echo -e "\n\e[92m    [✔] Sublime Text Installed\e[39m\n"
     else
         echo -e "\n\e[92m    [✔] Sublime Text is already installed\e[39m\n"
